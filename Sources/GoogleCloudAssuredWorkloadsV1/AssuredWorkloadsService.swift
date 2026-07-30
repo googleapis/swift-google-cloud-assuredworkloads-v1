0378da25a203cea3d5adfa7afe4cb772a895c335
Nolan Eastin <80856764+nolanleastin@users.noreply.github.com>
2026-07-30 16:48:45 -0700

    chore: update librarian circa 2026-07-30 (#76)
    chore: update librarian circa 2026-07-30
    
    In this version we get:
    - Configurable polling policies for clients with LROs
    - Fixed `$alt` query parameters for Compute
    - Restructured the storage generated protos

--- Sources/GoogleCloudAssuredWorkloadsV1/AssuredWorkloadsService.swift
+++ Sources/GoogleCloudAssuredWorkloadsV1/AssuredWorkloadsService.swift
@@ -28,6 +28,8 @@ import GoogleCloudGax
 /// @Snippet(path: "AssuredWorkloadsServiceQuickstart")
 public class AssuredWorkloadsServiceClient: Clients.AssuredWorkloadsServiceProtocol {
   let inner: any Clients.AssuredWorkloadsServiceStub
+  let pollingErrorPolicy: GoogleCloudGax.PollingErrorPolicy
+  let pollingBackoffPolicy: GoogleCloudGax.BackoffPolicy
 
   /// Creates a new `AssuredWorkloadsServiceClient` instance.
   public init(_ options: GoogleCloudGax.ClientOptions = .init()) throws {
@@ -38,6 +40,8 @@ public class AssuredWorkloadsServiceClient: Clients.AssuredWorkloadsServiceProto
       inner = Clients.AssuredWorkloadsServiceLogging(inner, logger: logger)
     }
     self.inner = inner
+    self.pollingErrorPolicy = options.pollingErrorPolicy
+    self.pollingBackoffPolicy = options.pollingBackoffPolicy
   }
 
   /// Creates Assured Workload.
@@ -100,7 +104,12 @@ public class AssuredWorkloadsServiceClient: Clients.AssuredWorkloadsServiceProto
         request: .init().with { $0.name = rawOp.name }, options: options)
       return try extractStatus(op)
     }
-    return GoogleCloudGax._PollableOperationImpl(initialState: initialState, poll: poll)
+    return GoogleCloudGax._PollableOperationImpl(
+      initialState: initialState,
+      polling: options.pollingErrorPolicy ?? self.pollingErrorPolicy,
+      backoff: options.pollingBackoffPolicy ?? self.pollingBackoffPolicy,
+      poll: poll,
+    )
   }
 
   /// Updates an existing workload.
